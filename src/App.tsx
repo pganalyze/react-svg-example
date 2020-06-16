3f62558baf02a31195add695a4ce4d26a29a43c6
Maciek Sakrejda <m.sakrejda@gmail.com>
2020-06-15 18:10:30 -0700

    Drop unneeded code

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Example } from './svg/Example';
 
 export const App: React.FC = () => {
-  return (
-    <div>
-      <Example />
-    </div>
-  );
+  return <Example />;
 }
 
 export default App;
